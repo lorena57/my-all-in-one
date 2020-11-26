4c744341e228cd6b94b6aee886a7649bd0370e5f
github username <github email address>
2020-11-26 14:02:50 -0800

    create store and reducers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 
+import { Provider } from "react-redux";
+import store from './store'
+
 import Posts from "./components/Posts";
 import Commentform from "./components/Commentform";
 
- class App extends Component {
+
+
+  class App extends Component {
   render() {
     return (
-      <div>
-        <Commentform/>
-        <hr/>
-        <Posts/>
-      </div>
+
+      <Provider store={store}>
+        <div>
+          <Commentform/>
+          <hr/>
+          <Posts/>
+        </div>
+      </Provider>
     )
   }
 }
--- src/components/Commentform.js
+++ src/components/Commentform.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+
+
 class Commentform extends Component {
     constructor(props) {
         super(props);
