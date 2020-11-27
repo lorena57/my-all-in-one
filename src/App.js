7c33ed12c9e6b886c2022ca08387a933001ed38a
github username <github email address>
2020-11-26 23:29:12 -0800

    add switch and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
-
-
-
 import Comments from "./components/Comments";
 import CommentForm from "./components/CommentForm";
-
+import ContactPage from "./containers/ContactPage";
+import GroceryPage from "./containers/GroceryPage";
 import IntroPage from "./containers/IntroPage";
 import Nav from './components/Nav';
 
@@ -16,12 +14,19 @@ import Nav from './components/Nav';
 
      
         <div>
-        <Nav />
+          <Nav />
 
-          <IntroPage/>
-          <CommentForm/>
+          <Switch>
+            <Route exact path='/' component={IntroPage} />
+            <Route path='/grocery' component={GroceryPage} />
+            {/* <Route path='/note' component={NotePage} /> */}
+            <Route path='/contact' component={ContactPage} />
+          </Switch>
+
+          
+          {/* <CommentForm/> */}
           <hr/>
-          <Comments/>
+          {/* <Comments/> */}
         </div>
      
     )
---
+++ src/containers/ContactPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Your contacts</h1>
+        </div>
+    )
+}
+export default ContactPage;
